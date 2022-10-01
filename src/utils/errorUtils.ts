9c2bc723e7deca78075062130971775a9a036aa2
Tiago <tigolimab223@gmail.com>
2022-10-01 00:05:12 -0300

    :sparkles: feat: Add isAppError function in errorUtils.ts

--- src/utils/errorUtils.ts
+++ src/utils/errorUtils.ts
@@ -10,6 +10,10 @@ export interface AppError {
   message: string;
 }
 
+export function isAppError(error: object): error is AppError {
+  return (error as AppError).type !== undefined;
+}
+
 export function errorTypeToStatusCode(type: AppErrorTypes) {
   if (type === "conflict") return 409;
   if (type === "not_found") return 404;
