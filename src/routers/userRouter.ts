45349c1b623d8aa1ebe266c9dd84e44258fe4011
Tiago <tigolimab223@gmail.com>
2022-10-01 01:44:23 -0300

    :bug: fix: added the userSchema in the validateSchemaMiddleware function

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getUserById, postUser } from "../controllers/userController.js";
 import { validateSchemaMiddleware } from "../middlewares/validateSchemaMiddleware.js";
+import { userCreateSchema } from "../schemas/userSchema.js";
 
 const userRouter = Router();
 
 userRouter.get("/users/:id", getUserById);
-userRouter.post("/users", validateSchemaMiddleware, postUser);
+userRouter.post("/users", validateSchemaMiddleware(userCreateSchema), postUser);
 
 export default userRouter;
