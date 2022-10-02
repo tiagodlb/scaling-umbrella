85a763c25a0ab1b9d3f739993307e9048e1f0df6
Tiago <tigolimab223@gmail.com>
2022-10-02 00:18:37 -0300

    :sparkles: feat: Add survey controller

---
+++ src/controllers/surveyController.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from 'express';
+import * as surveyService from "../services/surveyService.js";
+
+export async function getSurveys(req: Request, res: Response) {
+    const { id_user } = res.locals;
+
+    const data = await surveyService.getAllSurveys(id_user);
+
+    res.status(200).json(data)
+}
+
+export async function getSurveyById(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const data = await surveyService.getSurveyById(id);
+
+    res.status(200).json(data);
+}
+
+export async function postSurvey(req: Request, res: Response) {
+    const { id_user } = res.locals;
+
+    const survey = req.body;
+
+    const data = await surveyService.createSurvey({
+        id_user: id_user,
+        ...survey
+    })
+
+    res.status(201).json(data);
+}
+
+export async function putSurvey(req: Request, res: Response){
+    const { id_user } = res.locals;
+    const survey = req.body;
+
+    const data = await surveyService.updateSurvey(survey, id_user);
+
+    res.status(200).json(data);
+}
+
+export async function deleteSurvey(req: Request, res: Response){
+    const { id_user } = res.locals;
+    const survey = req.body;
+
+    const data = await surveyService.deleteSurvey(survey, id_user);
+
+    res.sendStatus(204);
+}
