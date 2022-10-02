f948035a856ac104400e5e988b4b9a950b05d871
Tiago <tigolimab223@gmail.com>
2022-10-01 23:48:42 -0300

    :sparkles: feat: Add a function that updates a survey

--- src/repositories/surveyRepository.ts
+++ src/repositories/surveyRepository.ts
@@ -2,6 +2,7 @@ import { Surveys } from "@prisma/client";
 import { prisma } from "../database/prisma.js";
 
 export type TCreateSurvey = Omit<Surveys, "id" | "created_at">;
+export type TUpdateSurvey = Partial<Surveys>;
 
 export async function getSurveyById(id: string): Promise<Surveys | null> {
   return await prisma.surveys.findUnique({
@@ -22,5 +23,15 @@ export async function createSurvey({
 }: TCreateSurvey): Promise<Surveys> {
   return await prisma.surveys.create({
     data: { id_user, name, description },
+  });
+}
+
+export async function updateSurvey(
+  survey: TUpdateSurvey,
+  id: string
+): Promise<Surveys> {
+  return await prisma.surveys.update({
+    where: { id },
+    data: survey,
   });
 }
