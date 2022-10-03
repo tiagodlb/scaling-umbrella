811bfe0523dc1be324debb1b299cc62ca7e57416
Tiago <tigolimab223@gmail.com>
2022-10-02 23:18:21 -0300

    :bug: bugfix: Changed the way the schema need to be filled

--- src/schemas/surveySchema.ts
+++ src/schemas/surveySchema.ts
@@ -2,6 +2,5 @@ import joi from "joi";
 
 export const surveySchema = joi.object({
   name: joi.string().min(5).max(100).required(),
-  description: joi.string().optional(),
-  password: joi.string().trim().required(),
+  description: joi.string().default("").optional().allow(""),
 });
