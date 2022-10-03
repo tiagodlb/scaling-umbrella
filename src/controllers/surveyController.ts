e25f800c816c3353cb734747d3e0184d7df16063
Tiago <tigolimab223@gmail.com>
2022-10-02 23:19:51 -0300

    :recycle: refactor: changed the way the functions works in order to match the changes in the service and routers

--- src/controllers/surveyController.ts
+++ src/controllers/surveyController.ts
@@ -1,49 +1,48 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 import * as surveyService from "../services/surveyService.js";
 
 export async function getSurveys(req: Request, res: Response) {
-    const { id_user } = res.locals;
+  const { id_user } = res.locals;
 
-    const data = await surveyService.getAllSurveys(id_user);
+  const data = await surveyService.getAllSurveys(id_user);
 
-    res.status(200).json(data)
+  res.status(200).json(data);
 }
 
 export async function getSurveyById(req: Request, res: Response) {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    const data = await surveyService.getSurveyById(id);
+  const data = await surveyService.getSurveyById(id);
 
-    res.status(200).json(data);
+  res.status(200).json(data);
 }
 
 export async function postSurvey(req: Request, res: Response) {
-    const { id_user } = res.locals;
+  const { id_user } = res.locals;
 
-    const survey = req.body;
+  const survey = req.body;
 
-    const data = await surveyService.createSurvey({
-        id_user: id_user,
-        ...survey
-    })
+  const data = await surveyService.createSurvey({
+    id_user: id_user,
+    ...survey,
+  });
 
-    res.status(201).json(data);
+  res.status(201).json(data);
 }
 
-export async function putSurvey(req: Request, res: Response){
-    const { id_user } = res.locals;
-    const survey = req.body;
+export async function putSurvey(req: Request, res: Response) {
+  const id: string = req.params.id;
+  const survey = req.body;
 
-    const data = await surveyService.updateSurvey(survey, id_user);
+  const data = await surveyService.updateSurvey(survey, id);
 
-    res.status(200).json(data);
+  res.status(200).json(data);
 }
 
-export async function deleteSurvey(req: Request, res: Response){
-    const { id_user } = res.locals;
-    const survey = req.body;
+export async function deleteSurvey(req: Request, res: Response) {
+  const id: string = req.params.id;
 
-    const data = await surveyService.deleteSurvey(survey, id_user);
+  await surveyService.deleteSurvey(id);
 
-    res.sendStatus(204);
+  res.sendStatus(204);
 }
