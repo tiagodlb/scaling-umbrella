6c432f13cfc13a2247dd01f5097d42b834fd89a5
tiagodlb <tigolimab223@gmail.com>
2022-10-10 15:11:07 -0300

    deploy

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import "express-async-errors";
 import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware";
 import router from "./routers/index";
 
-const app = express();
+const app = express(); 
 app.use(cors(), json());
 app.use(router);
 app.use(errorHandlerMiddleware);
