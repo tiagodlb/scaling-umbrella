9aca25b7e080807e5d939c4054d4d2bf9f0ef0b2
tiagodlb <tigolimab223@gmail.com>
2022-10-10 15:40:52 -0300

    deploy

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
-import dotenv from "dotenv";
-import app from "./app.js";
-
+import dotenv from 'dotenv';
+import app from './app.js';
 dotenv.config();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))  
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
