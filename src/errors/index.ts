1ae01babfd9ab6423e90ce5de47f303241f4a68b
tiagodlb <tigolimab223@gmail.com>
2022-11-10 13:40:21 -0300

    :wrench: chore: using the new errors

---
+++ src/errors/index.ts
@@ -0,0 +1,5 @@
+import * as invalidDataError from './invalid-data-error';
+import * as conflictError from './conflict-error';
+import * as emailError from './invalid-email-error'
+import * as notFoundError from './not-found-error';
+import * as unauthorizedError from './not-found-error';
