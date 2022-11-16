285beae343538e4394a90b518b49913c93c82c08
tiagodlb <tigolimab223@gmail.com>
2022-11-15 22:04:03 -0300

    feature: refactoring the code

---
+++ src/middlewares/error-handler-middleware.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response } from "express";
+import httpStatus from 'http-status';
+import { ApplicationError } from "../protocol";
+
+export default function errorHandlerMiddleware(
+  err: Error | ApplicationError,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) {
+  if (err.name === 'CannotEnrollBeforeStartDateError') {
+    return res.status(httpStatus.BAD_REQUEST).send({
+      message: err.message,
+    });
+  }
+
+  if (err.name === 'ConflictError' || err.name === 'DuplicatedEmailError') {
+    return res.status(httpStatus.CONFLICT).send({
+      message: err.message,
+    });
+  }
+
+  if (err.name === 'InvalidCredentialsError') {
+    return res.status(httpStatus.UNAUTHORIZED).send({
+      message: err.message,
+    });
+  }
+
+  if (err.name === 'NotFoundError') {
+    return res.status(httpStatus.NOT_FOUND).send({
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+  res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
+    error: 'InternalServerError',
+    message: 'Internal Server Error',
+  });
+}
