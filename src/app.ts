d52ef575fa264267d07a9dd176ec886df8a69028
tiagodlb <tigolimab223@gmail.com>
2022-11-15 22:52:42 -0300

    fix: paths

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
 import cors from "cors";
-import express, { json } from "express";
+import express, { json, Express } from "express";
 import "express-async-errors";
-import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware";
+import { connectDB, disconnectDB } from "./config/database";
+import { loadEnv } from "./config/envs";
+import errorHandlerMiddleware from "./middlewares/error-handler-middleware";
 import router from "./routers/index";
 
-const app = express(); 
-app.use(cors(), json());
-app.use(router);
-app.use(errorHandlerMiddleware);
- 
+loadEnv();
+
+const app = express();
+app
+    .use(cors(), json())
+    .use('/health', (_req, res) => res.send('Everything is OK'))
+    .use(router)
+    .use(errorHandlerMiddleware);
+
+export function init(): Promise<Express> {
+    connectDB();
+    return Promise.resolve(app)
+}
+
+export async function close(): Promise<void> {
+    await disconnectDB();
+}
+
 export default app;
